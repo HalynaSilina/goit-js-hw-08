5784f74bb54d0ac635854fb0fa47b4210949aa35
HalynaSilina <halynasilina@gmail.com>
2023-03-09 20:58:41 +0200

    form storage

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,26 +1,26 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-import SimpleLightbox from "simplelightbox";
-import "simplelightbox/dist/simple-lightbox.min.css";
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
-const galleryContainer = document.querySelector(".gallery");
+const galleryContainer = document.querySelector('.gallery');
 
 const galleryImagesMarkup = galleryItems
   .map(
     ({ preview, original, description }) =>
       `<a class="gallery__item" href="${original}"><img class="gallery__image" src="${preview}", alt="${description}"/></a>`
   )
-  .join(" ");
+  .join(' ');
 
-galleryContainer.insertAdjacentHTML("beforeend", galleryImagesMarkup);
+galleryContainer.insertAdjacentHTML('beforeend', galleryImagesMarkup);
 
-const gallery = new SimpleLightbox(".gallery a", {
+const gallery = new SimpleLightbox('.gallery a', {
   captions: true,
-  captionSelector: "img",
-  captionType: "attr",
-  captionsData: "alt",
-  captionPosition: "bottom",
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
   captionDelay: 250,
 });
